f23dff248e1d981b5799b527c2ccadc6eff01b62
sergey12malyshev <59499846+sergey12malyshev@users.noreply.github.com>
2023-07-19 22:20:07 +0500

    Добавление в проект управление через телеграмм бот

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ESP32_LED_ADR
-Тестовый проект для ESP32. Доработанная демоверсия примера DemoReel100 управления адресной LED-лентой
+Проект управления адресной светодиодной LED-лентой типа WS2812 через чат-бот Telegramm. 
+
+Основана на библиотеках FastBot и FastLed
 
 ## Инструментарий
 *PlatformIO* + *VS Code*
@@ -8,9 +10,22 @@
 - ESP32_DevKitc_V4
 - WS2812
 
+## Подключение
+Создать файл *pass.h* и добавить в проект:
+```
+ #define WIFI_SSID "XXXX"
+ #define WIFI_PASS "XXXXXXXXX"
+ #define BOT_TOKEN "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+ #define CHAT_ID  "XXXXXXXXXXXXXXX"
+```
+
 PinOut: https://www.electronshik.ru/news/show/12935?from=terraelectronica
 
 ## Полезная информация
-Исходник: https://github.com/FastLED/FastLED/blob/master/examples/DemoReel100/DemoReel100.ino
+Исходник FastLed: https://github.com/FastLED/FastLED/blob/master/examples/DemoReel100/DemoReel100.ino
+
+Библиотека FastBot: https://kit.alexgyver.ru/tutorials/fastbot/
+
+Создание бота Telegramm: https://kit.alexgyver.ru/tutorials/telegram-basic/  https://habr.com/ru/articles/729246/
 
 Настройка VS Code + PlatformIO: https://randomnerdtutorials.com/vs-code-platformio-ide-esp32-esp8266-arduino/
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 /******************************************************************************/
-/* Test project based on FastLED library and DemoReel100.ino project          */
-/* Malyshev S.E.                                                              */
+/* Test project based on FastLED library and DemoReel100.ino project 
+/* Added control via Telegramm bot
+/* Malyshev S.E.                                                              
 /******************************************************************************/
 #include <Arduino.h>
 #include <FastLED.h>
+#include <FastBot.h>
+#include <pass.h> // Файл содержит логин/пароль WiFi + bot/id токен
+
+#define DEBUG    false         //Включить отладочные сообщения
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
@@ -18,6 +23,18 @@ const uint8_t brightness = 96u;
 const uint8_t framesPerSecond = 120u;
 const uint8_t voltage_v = 5u;
 const uint8_t current_mA = 120u;
+/*
+Команды для телеграмм бота
+next - сменить режим
+led_on - включить ленту
+led_off - выключить ленту
+test - тест связи
+help - доступные команды
+hardreset - перезапуск платы
+*/
+typedef enum {NOWORK = 0, WORK, PATTERN} modes;
+modes mainModes = WORK;
+FastBot bot(BOT_TOKEN);
 
 CRGB leds[NUM_LEDS];
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
@@ -36,6 +53,11 @@ static void staticRandomColor(void);
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
 SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm, staticRandomColor};
+
+static void disableLEDs(void) // Выключение
+{
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+}
 
 static void nextPattern(void)
 {
@@ -70,7 +92,7 @@ static void confetti(void)
 static void sinelon(void)
 {
   // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
+  fadeToBlackBy(leds, NUM_LEDS, 20);
   int pos = beatsin16( 13, 0, NUM_LEDS-1);
   leds[pos] += CHSV( gHue, 255, 192);
 }
@@ -113,7 +135,7 @@ static void staticRandomColor(void)
   }
 }
 
-void setup() 
+static void setupFastLed() 
 {
   // tell FastLED about the LED strip configuration
   delay(250);
@@ -122,13 +144,117 @@ void setup()
   FastLED.setBrightness(brightness);
 }
 
-void loop()
+static void ledProcess()
 {
   gPatterns[gCurrentPatternNumber]();
   FastLED.show();
   FastLED.delay(1000/framesPerSecond);
 
-  /*do some periodic updates */
+  //do some periodic updates
   EVERY_N_MILLISECONDS(20) { gHue++; } // slowly cycle the "base color" through the rainbow
+}
+
+static void loopFastLed()
+{
+  ledProcess();
   EVERY_N_SECONDS(10) { nextPattern(); } // change patterns periodically
+}
+
+// обработчик сообщений telegramm
+static void newMsg(FB_msg& msg) 
+{
+#if DEBUG  
+  // выводим ID чата, имя юзера и текст сообщения
+  Serial.print(msg.chatID);     // ID чата 
+  Serial.print(", ");
+  Serial.print(msg.username);   // логин
+  Serial.print(", ");
+  Serial.println(msg.text);     // текст
+#endif
+
+  if (msg.text == "/test@LedLigthBot")
+  {
+    bot.sendMessage("Test OK!", msg.chatID);
+  } 
+  else if (msg.text == "/led_on@LedLigthBot") 
+  {
+    mainModes = WORK;
+    bot.sendMessage("LED enabled!", msg.chatID);
+  } 
+  else if (msg.text == "/led_off@LedLigthBot") 
+  {
+    mainModes = NOWORK;
+    FastLED.clear(true);
+    bot.sendMessage("LED disabled!", msg.chatID);
+  }
+  else if (msg.text == "/next@LedLigthBot") 
+  {
+    mainModes = PATTERN;
+    bot.sendMessage("next pattern", msg.chatID);
+    nextPattern();
+  }
+  else if (msg.text == "/help@LedLigthBot")
+  {
+    bot.sendMessage("Я умею:\r\n\
+    next - сменить режим\r\n\
+    led_on - включить ленту\r\n\
+    led_off - выключить ленту\r\n\
+    test - тест связи\r\n\
+    hardreset - перезапуск платы\r\n\
+    help - доступные команды", msg.chatID);
+  }
+  else if (msg.text == "/hardreset@LedLigthBot")
+  {
+    bot.sendMessage("ПЕРЕЗАГРУЖАЮСЬ...", msg.chatID);                          
+    delay(1000);                                                               
+    bot.tickManual();
+    ESP.restart();                                                              
+  }
+  else
+  {
+    bot.sendMessage("я не знаю такой команды...", msg.chatID);   
+  }
+}
+
+static void connectWiFi() 
+{
+  delay(2000);
+  Serial.begin(115200);
+  Serial.println();
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+    if (millis() > 15000) ESP.restart();
+  }
+  Serial.println("Connected");
+}
+
+void setup() 
+{
+  setupFastLed();
+
+  connectWiFi();
+  bot.attach(newMsg);
+
+  bot.setChatID(CHAT_ID);
+  bot.sendMessage("Power On");
+}
+
+void loop() 
+{
+  bot.tick();
+
+  switch (mainModes)
+  {
+    case WORK:
+      loopFastLed();
+      break;
+    case PATTERN:
+      ledProcess();
+      break;
+    default:
+      break;
+  }
 }
